ec9188a6e4ac38fad28286ee1d6659d141986f14
Eduardo gonçalves <eduardogoncalves3692@gmail.com>
2023-11-28 21:29:37 -0300

    add checkGameWIn

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,13 +1,14 @@
 function backPage() {
-    const playerConfirm = confirm("Deseja realmente sair? Voçê perderá seu progresso");
-    if (playerConfirm) {
-        window.history.back();// 👈👌 Código TopZeira autofocus //
-    }
+  const playerConfirm = confirm(
+    "Deseja realmente sair? Voçê perderá seu progresso"
+  );
+  if (playerConfirm) {
+    window.history.back(); // 👈👌 Código TopZeira autofocus //
+  }
 }
-// FAzzz o LLL
 
 function createCards() {
-    const cardNames = [
+  const cardNames = [
     "card-1",
     "card-2",
     "card-3",
@@ -15,15 +16,27 @@ function createCards() {
     "card-5",
     "card-6",
     "card-7",
-    ];
+    "card-8",
+    "card-9",
+    "card-10",
+    "card-11",
+    "card-12",
+    "card-13",
+    "card-14",
+    "card-15",
+    "card-16",
+    "card-17",
+  ];
 
-    const arrayCardsName = cardNames
-        .sort(() => Math.random() - 0.5)
-        .filter((value, index) => index < 7);
-    const sortedCards = [...arrayCardsName,...arrayCardsName].sort(() => Math.random() - 0.5);
-    gridCards.innerHTML = "";
-    sortedCards.forEach((card) => {
-        gridCards.innerHTML += `
+  const arrayCardsName = cardNames
+    .sort(() => Math.random() - 0.5)
+    .filter((value, index) => index < 8);
+  const sortedCards = [...arrayCardsName, ...arrayCardsName].sort(
+    () => Math.random() - 0.5
+  );
+  gridCards.innerHTML = "";
+  sortedCards.forEach((card) => {
+    gridCards.innerHTML += `
         <div class="card" name="${card}">
             <div class="front">
                 <img src="../images/${card}.jpg" alt="">
@@ -32,53 +45,112 @@ function createCards() {
                 <img src="../images/yugioh-card-back.png" alt="">
             </div>
         </div>
-        `
-    });
+        `;
+  });
 }
 
 function clickFlipCard() {
-    const arrayCards = document.querySelectorAll(".card");
-    arrayCards.forEach((card) => {
-        card.addEventListener("click", () => {
-            console.log("Da uma apertadinha aq no pai");
-            if (card.classList.contains("flipCard")) return;
-            new Audio("../audios/flip.wav").play();
-            if (firstCard === "") {
-                card.classList.add("flipCard");
-                firstCard = card;
-            } else if (secondCard === "") {
-                card.classList.add("flipCard");
-                secondCard = card;
+  const arrayCards = document.querySelectorAll(".card");
+  arrayCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      console.log("Da uma apertadinha aq no pai");
+      if (card.classList.contains("flipCard")) return;
+      new Audio("../audios/flip.wav").play();
+      if (firstCard === "") {
+        card.classList.add("flipCard");
+        firstCard = card;
+      } else if (secondCard === "") {
+        card.classList.add("flipCard");
+        secondCard = card;
 
-                checkMatchCards();
-            }   
-        })
-    })
+        checkMatchCards();
+      }
+    });
+  });
+}
+
+function setStartTimer() {
+
+    finishTimerInterval = setInterval(() => {
+  
+
+  const dateNow = new Date();
+  const dateDiff = new Date(dateNow - initialDateTimer)
+
+const minutes = 
+String(dateDiff.getMinutes()).padStart("2", "0");
+const seconds = 
+String(dateDiff.getSeconds()).padStart("2", "0");
+
+console.log(`${String(minutes)}:${seconds}`);
+
+timer.innerHTML = `${minutes} : ${seconds}`; 
+}, 1000);
 
 }
 
-function checkMatchCards() {
-    if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
-        new Audio("../audios/sci-fi.wav").play();
-        setTimeout(() => {
-            firstCard.classList.add("disabledCard");
-            secondCard.classList.add("disabledCard");
-            firstCard = ""; secondCard = "";
-        }, 500)
-        
+function checkGamewin() {
+    const disabledCards = document.querySelectorAll(".disabledCard");
+    console.log(disabledCards);
+    if (disabledCards.length === 2) {
+      clearInterval(finishTimerInterval);
+
+const userData = {
+    name : storagePlayerNick,
+    time : timer.textContent,
+};
+
+// usamos JSON parse para transformar a string em algo que JS possa trabalhar, ou seja, object.
+
+    const storageRank = JSON.parse(localStorage.setItem("@memoryGame:rank", userData));
+
+    if (storageRank) {
+        console.log(storageRank);
+        const rankData = [...storageRank, userData];
+
+        localStorage.setItem("@memoryGame:rank",JSON.stringify([rankData]));
+
     } else {
-        setTimeout(() => {
-            firstCard.classList.remove("flipCard");
-            secondCard.classList.remove("flipCard");
-            firstCard = ""; secondCard = "";
-        }, 500)
-        
+
+        localStorage.setItem("@memoryGame:rank",JSON.stringify([userData]));
+
     }
+   
+
+    alert(`Parabéns ${storagePlayerNick}, você ganhou com o tempo de: 00:00!`);
+  }
+}
+
+function checkMatchCards() {
+  // se ambas as cartas tiverem o mesmo atributo ou imagem
+  if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
+    // O aúdio funciona
+
+    new Audio("../audios/sci-fi.wav").play();
+    setTimeout(() => {
+      firstCard.classList.add("disabledCard");
+      secondCard.classList.add("disabledCard");
+      firstCard = "";
+      secondCard = "";
+    }, 500);
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("flipCard");
+      secondCard.classList.remove("flipCard");
+      firstCard = "";
+      secondCard = "";
+
+      // pode checar nâo só para vencer o jogo, mas tambèm
+      // para checar se o player teve um numéro X de cartas,
+      // no caso 20
+    }, 500);
+  }
 }
 
 const playerNick = document.querySelector(".playerNick");
 const backButton = document.querySelector(".backButton");
 const gridCards = document.querySelector(".gridCards");
+const timer = document.querySelector(".playerTimer");
 
 const storagePlayerNick = localStorage.getItem("@memoryGame:playerName");
 playerNick.innerHTML = storagePlayerNick;
@@ -89,3 +161,9 @@ createCards();
 let firstCard = "";
 let secondCard = "";
 clickFlipCard();
+
+const initialDateTimer = new Date();
+let finishTimerInterval;
+setStartTimer();
+
+
